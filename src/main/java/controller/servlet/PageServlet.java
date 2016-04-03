ab86d0bf119e33ea7211679d14d5bf5a814e52c8
Ilya Khudyakov <mra4niykeks@gmail.com>
2016-04-03 19:18:03 +0300

    init

---
+++ src/main/java/controller/servlet/PageServlet.java
@@ -0,0 +1,26 @@
+package controller.servlet;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Business_Book on 03.04.2016.
+ */
+public class PageServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            RequestDispatcher rd = request.getRequestDispatcher("/pages/mainPage.jsp");
+            rd.include(request, response);
+            System.out.println(request.getRequestURL());
+        } catch (Exception e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+}
